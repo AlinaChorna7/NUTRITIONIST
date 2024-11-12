ede9e050df5b6fa8497b0723c0086429599b048c
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-11-12 21:07:04 +0100

    upd  menu

--- src/componens/app.jsx
+++ src/componens/app.jsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import './index.css';
+import '../../src/css/styles.css';
 import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 
 export default function App() {
